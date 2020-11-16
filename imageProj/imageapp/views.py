7ee59f4cb862e34b28c9155a97ccc109e3bd3693
oluwatobi1 <akintunlesetobi@gmail.com>
2020-11-16 18:01:31 +0100

    changes in views for model setup

--- imageProj/imageapp/views.py
+++ imageProj/imageapp/views.py
@@ -12,55 +12,56 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 
 
-
-from keras.models import load_model
-from keras.preprocessing import image
-import json
-from tensorflow import Graph
-import tensorflow as tf
-from tensorflow.keras.applications import EfficientNetB6
-from tensorflow import keras
-from tensorflow.keras.models import Model
-from tensorflow.keras import layers
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout, GlobalAveragePooling2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ModelCheckpoint
-import os, cv2
-
-
-ROWS = 256
-COLS = 256
-Channel = 3
-
+#
+# from keras.models import load_model
+# from keras.preprocessing import image
+# import json
+# from tensorflow import Graph
+# import tensorflow as tf
+# from tensorflow.keras.applications import EfficientNetB6
+# from tensorflow import keras
+# from tensorflow.keras.models import Model
+# from tensorflow.keras import layers
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.layers import Input, Dense, BatchNormalization, Dropout, GlobalAveragePooling2D
+# from tensorflow.keras.optimizers import Adam
+# from tensorflow.keras.callbacks import ModelCheckpoint
+# import os, cv2
 
 #
-model_graph = Graph()
-with model_graph.as_default():
-    tf_session = tf.compat.v1.Session()
-    with tf_session.as_default():
-        # create the base pre-trained model
-        base_model = EfficientNetB6(weights='imagenet', include_top=False, input_tensor=Input(shape=(ROWS, COLS, 3)))
-        x = base_model.output
-        x = GlobalAveragePooling2D()(x)
-        # # let's add a fully-connected layer
-        x = Dense(2024, activation='relu')(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.2)(x)
-        x = Dense(512, activation='relu')(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.2)(x)
-        x = Dense(128, activation='relu')(x)
-        x = BatchNormalization()(x)
-        x = Dropout(0.1)(x)
-        # and a logistic layer -- let's say we have 200 classes
-        predictions = Dense(2, activation='softmax')(x)
-        # this is the model we will train
-        model = Model(inputs=base_model.input, outputs=predictions)
-        # The model weights (that are considered the best) are loaded into the model
+# ROWS = 256
+# COLS = 256
+# Channel = 3
+#
 
-# # to read an image we need the directory and it can be jpeg or png
-# print(preds,)
+#
+# model_graph = Graph()
+# with model_graph.as_default():
+#     tf_session = tf.compat.v1.Session()
+#     with tf_session.as_default():
+#         # create the base pre-trained model
+#         base_model = EfficientNetB6(weights='imagenet', include_top=False, input_tensor=Input(shape=(ROWS, COLS, 3)))
+#         x = base_model.output
+#         x = GlobalAveragePooling2D()(x)
+#         # # let's add a fully-connected layer
+#         x = Dense(2024, activation='relu')(x)
+#         x = BatchNormalization()(x)
+#         x = Dropout(0.2)(x)
+#         x = Dense(512, activation='relu')(x)
+#         x = BatchNormalization()(x)
+#         x = Dropout(0.2)(x)
+#         x = Dense(128, activation='relu')(x)
+#         x = BatchNormalization()(x)
+#         x = Dropout(0.1)(x)
+#         # and a logistic layer -- let's say we have 200 classes
+#         predictions = Dense(2, activation='softmax')(x)
+#         # this is the model we will train
+#         model = Model(inputs=base_model.input, outputs=predictions)
+#         # The model weights (that are considered the best) are loaded into the model
+
+
+
+
 # print('Confidence percentile %s %% ,it is Predicted: %s' %(round(max(preds[0][0:2]),2),round(preds[0][0],0)))
 # ```
 
@@ -77,20 +78,20 @@ def predict_image(request):
     fs = FileSystemStorage()
     filePathName = fs.save(fileObj.name, fileObj)
     filePathName = fs.url(filePathName)
-    test_image='.'+filePathName
-
-    img = cv2.imread(test_image, cv2.IMREAD_COLOR)
-     #cv2.IMREAD_GRAYSCALE
-    img = cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)
-    image = tf.reshape(img,(-1, ROWS, COLS,3))
-    with model_graph.as_default():
-        with tf_session.as_default():
-            preds = model.predict(image, steps=1) # Predict image
-
-    pred_label = round(preds[0][0],0)
-    confidence=round(max(preds[0][0:2]),2)
-
-    context ={"filePathName": filePathName, "prediction": pred_label, "Confidence":confidence}
+    # test_image='.'+filePathName
+    #
+    # img = cv2.imread(test_image, cv2.IMREAD_COLOR)
+    #  #cv2.IMREAD_GRAYSCALE
+    # img = cv2.resize(img, (ROWS, COLS), interpolation=cv2.INTER_CUBIC)
+    # image = tf.reshape(img,(-1, ROWS, COLS,3))
+    # with model_graph.as_default():
+    #     with tf_session.as_default():
+    #         preds = model.predict(image, steps=1) # Predict image
+    #
+    # pred_label = round(preds[0][0],0)
+    # confidence=round(max(preds[0][0:2]),2)
+###change "prediction"
+    context ={"filePathName": filePathName, "prediction": "pred_label", "Confidence":"confidence"}
     return render(request, 'index.html', context)
 
 
